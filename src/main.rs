f42e05b9c33d830f0b219673db75e4812d12b503
root <gao.ten@northeastern.edu>
2024-12-19 07:24:33 +0000

    test commit in container

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io;
 fn main() -> io::Result<()> {
     // see if tun_tap working
 
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("failed to cr")?;
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 
     // create a new interface
     let mut buf = [0u8; 1504];
