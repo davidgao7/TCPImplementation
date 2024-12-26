b024c640d58a7befd86c603a00f61fe7ee9bc33e
David Gao <gao.ten@northeastern.edu>
2024-12-25 21:00:05 -0500

    capture the packet , next parse valueble info

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-extern crate tun_tap;  // Declares the use of the tun_tap crate, which provides bindings for creating and managing TUN/TAP interfaces.
-extern crate hex; // hexdecimal encoding/decoding
+extern crate hex;
+extern crate tun_tap; // Declares the use of the tun_tap crate, which provides bindings for creating and managing TUN/TAP interfaces. // hexdecimal encoding/decoding
 
 use std::io;
 //use hex_fmt::HexFmt;
@@ -7,9 +7,9 @@ use std::io;
 fn main() -> io::Result<()> {
     // see if tun_tap working
     /*
-    * Ok(()) : Success
-    * Err(io::Error) : an error from the standard I/O library
-    * */
+     * Ok(()) : Success
+     * Err(io::Error) : an error from the standard I/O library
+     * */
 
     //print!("======run till here 0=============\n");
     // create a TUN interface
@@ -23,34 +23,56 @@ fn main() -> io::Result<()> {
     // create a new interface
     // this buffer will hold the incoming packet data from the TUN interface
     let mut buf = [0u8; 1504]; // create a buffer of size 1504 bytes
-    //print!("======run till here 2=============\n");
+                               //print!("======run till here 2=============\n");
 
     // receiving data
     // this is a **blocking operation**, the program will wait until a packet is received
     // the process repeats for every incoming packet, continuously listening for data on `tun0`
-    loop{
-        let nbytes = nic.recv(&mut buf[..])?;  // Receives a packet from the interface(when a packet arrives), return #bytes copied into the buffer
-        //print!("======run till here 3=============\n");
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?; // Receives a packet from the interface(when a packet arrives), return #bytes copied into the buffer
+                                              //print!("======run till here 3=============\n");
 
         /*
-        * u16::from_be_bytes
-        * constructs a `u16` integer from a 2-element byte array interpreted in big-endian
-        * (network) byte order. This means the most significant byte is at the first position
-        * of the array
-        *
-        * Parameters:
-        * `bytes`: a `[u8; 2] array representing the bytes in big-endian order
-        *
-        * Output
-        * returns a `u16` integer composed from the provided byte array*/
+         * u16::from_be_bytes
+         * constructs a `u16` integer from a 2-element byte array interpreted in big-endian
+         * (network) byte order. This means the most significant byte is at the first position
+         * of the array
+         *
+         * Parameters:
+         * `bytes`: a `[u8; 2] array representing the bytes in big-endian order
+         *
+         * Output
+         * returns a `u16` integer composed from the provided byte array*/
         // the first 4 bytes of the buffer ar einterpreted as metadata
         let flags = u16::from_be_bytes([buf[0], buf[1]]); // byte 0-1: represent flags (interpreted as a u16 integer in big-endian order)
         let proto = u16::from_be_bytes([buf[2], buf[3]]); // protocol type (e.g. IPv4, IPv6)
 
-        // nbytes: the number of bytes read into buffer
-        // &buf[..nbytes] : a slice of the buffer containing only the received data
-        // {:x} : attempts to format the buffer slice as hexadecimal
-        //print!("======run till here 4=============\n");
-        eprintln!("read {} bytes (flags: {:x}, proto: {:x}: {:x?})", nbytes - 4, flags, proto, &buf[4..nbytes]);
+        // filter anything that is not ipv4 packet
+        if proto != 0x0800 {
+            // no ipv4
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                // nbytes: the number of bytes read into buffer
+                // &buf[..nbytes] : a slice of the buffer containing only the received data
+                // {:x} : attempts to format the buffer slice as hexadecimal
+                //print!("======run till here 4=============\n");
+                // NOTE: get proto type : 86dd , which is the enthertype that indicates the payload of an
+                // enthernet frame contains an IPV6 (internet protocol version 6) packet
+                eprintln!(
+                    "read {} bytes (flags: {:x}, proto: {:x}: {:x?})",
+                    nbytes - 4,
+                    flags,
+                    proto,
+                    p
+                );
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e)
+            }
+        }
     }
+    //Ok(())
 }
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e  # ensures the script exits if any command fails
+set -e # ensures the script exits if any command fails
 # ensure run.sh uses unix-style line endings (\n) instead of windows-style(\r\n)
 
 # 1. build the rust project
@@ -9,7 +9,7 @@ cargo b --release
 # setcap cap_net_admin=eip ../target/release/tcp_implementation
 
 # 3. run rust executable in background
-../target/release/tcp_implementation &
+target/release/tcp_implementation
 pid=$! # create a thread to wait response
 # 4. add tun0 ip addr
 ip addr add 192.168.0.1/24 dev tun0
@@ -18,5 +18,5 @@ ip link set up dev tun0
 # 5. check ip addr
 # ip addr
 # wait process to finish
-trap kill $pid INT TERM
+trap 'kill $pid' INT TERM # terminate program normally when use `C-c`
 wait $pid
